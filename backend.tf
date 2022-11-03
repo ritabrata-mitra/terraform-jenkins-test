9e1d5a8085960f63a2a28884d704340a22c0a0b2
ritabrata-mitra <105199843+ritabrata-mitra@users.noreply.github.com>
2022-11-03 13:13:39 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    encrypt = true    
-    bucket = "rito-test-bucket-terraform-jenkins-integration"
-    dynamodb_table = "terraform-state-lock-dynamo"
-    key    = "terraform.tfstate"
-    region = "eu-central-1"
-  }
-}
